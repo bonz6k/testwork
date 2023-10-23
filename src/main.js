0995ce6cb6bbdb6be1d1f4eba1ab2a5537a22c4b
Konstantin Bonz <konstantinbonz00@mail.ru>
2023-10-23 05:26:13 +0300

    update 1.0

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 //функция для сравнения двух текстовых полей
 function test() {
+    //инициализация переменных для хранения значений текстовых полей
     var txtAreaLeftValue = document.querySelector('#TextAreaLeft').value;
     var txtAreaRightValue = document.querySelector('#TextAreaRight').value;
     
+    //инициализация массивов для хранения строк
     let lineArrayLeft = new Array();
     let lineArrayRight = new Array();
 
+    //разбиение целого текстового значения на строки и запись строк в массив
     let splitLeft = txtAreaLeftValue.split('\n');
     let splitRight = txtAreaRightValue.split('\n');
 
+    //нумерация строк
     for (let i = 0; i < splitLeft.length; i++){
         lineArrayLeft[i] = i+1+'.  ' + splitLeft[i];
     }
@@ -16,33 +20,39 @@ function test() {
         lineArrayRight[i] = i+1+'.  ' + splitRight[i];
     }
 
-    const SpanElement = document.createElement("span")
+    //инициализация блоков, в которых отображается результат проверки
     let divLeft = document.getElementById('DivLeft');
     let divRight = document.getElementById('DivRight');
 
+    //цикл проверки для левого блока
     for(let i = 0; i<lineArrayLeft.length; i++){
-        const spanElement = document.createElement("span");
-        divLeft.appendChild(spanElement);
+        const spanElement = document.createElement("span"); //инициализация метода createElement()
+        divLeft.appendChild(spanElement); //создание дочернего span внутри divLeft
+        //если в левом текстовом поле есть проверяемая строка или строка пустая, то не подсвечиваем строку
         if((splitRight.indexOf(splitLeft[i]) != -1) || (splitLeft[i] == '')){
-            spanElement.innerText = lineArrayLeft[i];
-            spanElement.className = 'default';
+            spanElement.innerText = lineArrayRight[i]; //передаем span текстовое значение строки
+            spanElement.className = 'default'; //задаем для span параметры стиля
         }
+        //если проверяемой строки нет, то подсвечиваем строку
         else{
-            spanElement.innerText = lineArrayLeft[i];
-            spanElement.className = 'green';
+            spanElement.innerText = lineArrayRight[i]; //передаем span текстовое значение строки
+            spanElement.className = 'green'; //задаем для span параметры стиля
         }
     }
 
+    //цикл проверки для правого блока
     for(let i = 0; i<lineArrayRight.length; i++){
-        const spanElement = document.createElement("span");
-        divRight.appendChild(spanElement);
+        const spanElement = document.createElement("span"); //инициализация метода createElement()
+        divRight.appendChild(spanElement); //создание дочернего span внутри divRight
+        //если в левом текстовом поле есть проверяемая строка или строка пустая, то не подсвечиваем строку
         if((splitLeft.indexOf(splitRight[i]) != -1) || (splitRight[i] == '')){
-            spanElement.innerText = lineArrayRight[i];
-            spanElement.className = 'default';
+            spanElement.innerText = lineArrayRight[i]; //передаем span текстовое значение строки
+            spanElement.className = 'default'; //задаем для span параметры стиля
         }
+        //если проверяемой строки нет, то подсвечиваем строку
         else{
-            spanElement.innerText = lineArrayRight[i];
-            spanElement.className = 'red';
+            spanElement.innerText = lineArrayRight[i]; //передаем span текстовое значение строки
+            spanElement.className = 'red'; //задаем для span параметры стиля
         }
     }
 }
